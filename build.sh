fbdc4ee87cac92638c89e5f5f372a4517e315d3b
MagicianBoom <ly666888555@163.com>
2023-03-12 22:36:09 +0800

    增加ioctl函数的支持及对应app的支持

--- build.sh
+++ build.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+# 路径、密码等定义
 PASSWORD=orangepi
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 OUTPUT_FILE_DIR=$CURRENT_DIR/output_file
 SCP_TARGET_DIR=orangepi@192.168.0.104:/home/orangepi/workspace
 SCP_SOURCE_DIR=$OUTPUT_FILE_DIR
 
+clear & clear
+
+# 编译驱动
+cd $CURRENT_DIR
 make
 
-cp $CURRENT_DIR/*.ko $SCP_SOURCE_DIR
+# 编译APP
+cd $CURRENT_DIR/app
+make ARCH=arm64
 
+# 搬运生成的需要用的文件
+rm $SCP_SOURCE_DIR/*
+cp $CURRENT_DIR/*.ko $SCP_SOURCE_DIR
+cp $CURRENT_DIR/run.sh $SCP_SOURCE_DIR
+cp $CURRENT_DIR/app/build/lysi_app $SCP_SOURCE_DIR
+
+# scp文件传输
 sshpass -p ${PASSWORD} scp ${SCP_SOURCE_DIR}/* ${SCP_TARGET_DIR}
 
+# 清除生成的文件
+cd $CURRENT_DIR
+make clean
+cd $CURRENT_DIR/app
 make clean
--- lysi_core.c
+++ lysi_core.c
@@ -35,12 +35,49 @@ static int lysi_release(struct inode *inode, struct file *filp)
     return 0;
 }
 
+static long lysi_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+    int ret;
+
+    switch(cmd) {
+    case LYSI_IOCTL_CMD0:
+        printk("LY_DEBUG===> %s: ioctl cmd0\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD1:
+        printk("LY_DEBUG===> %s: ioctl cmd1\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD2:
+        printk("LY_DEBUG===> %s: ioctl cmd2\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD3:
+        printk("LY_DEBUG===> %s: ioctl cmd3\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD4:
+        printk("LY_DEBUG===> %s: ioctl cmd4\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD5:
+        printk("LY_DEBUG===> %s: ioctl cmd5\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD6:
+        printk("LY_DEBUG===> %s: ioctl cmd6\r\n", __func__);
+        return 0;
+    case LYSI_IOCTL_CMD7:
+        printk("LY_DEBUG===> %s: ioctl cmd7\r\n", __func__);
+        return 0;
+    default:
+        printk("LY_DEBUG===> %s: cmd error\r\n", __func__);
+    }
+
+    return ret;
+}
+
 static struct file_operations lysi_fops = {
     .owner = THIS_MODULE,
     .open = lysi_open,
     .read = lysi_read,
     .write = lysi_write,
     .release = lysi_release,
+    .unlocked_ioctl = lysi_ioctl,
 };
 
 static int lysi_probe(struct platform_device *dev)
@@ -54,7 +91,7 @@ static int lysi_probe(struct platform_device *dev)
         alloc_chrdev_region(&lysi_dev.devid, 0, LYSI_CNT, LYSI_NAME);
         lysi_dev.major = MAJOR(lysi_dev.devid);
     }
-    printk("newcheled major=%d,minor=%d\r\n", lysi_dev.major, lysi_dev.minor);
+    printk("LY_DEBUG===> lysi_dev major=%d,minor=%d\r\n", lysi_dev.major, lysi_dev.minor);
 
     /* 2、初始化 cdev */
     lysi_dev.cdev.owner = THIS_MODULE;
